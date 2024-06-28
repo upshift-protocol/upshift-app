30e4a8666221db3ada2779b5007a807d7732cc38
lazarev <lazarevcrypto@gmail.com>
2024-06-28 15:58:51 -0400

    retrying netlify 1

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -3,11 +3,9 @@ import { ReactNode } from "react";
 export type INavItem = {
   text: string;
   link: string;
-  target: "_blank" | "_self"
+  target: '_blank' | '_self';
 }
 
 export type IChildren = {
   children: ReactNode;
 }
-
-export type ITailwindClass = React.ComponentProps<'div'>['className'];;
