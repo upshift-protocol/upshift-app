6fe326d0beb676cf17043773bdfa891e93720337
lazarev <lazarevcrypto@gmail.com>
2024-07-01 08:04:02 -0400

    repnpm and also pnpm issue so theme switch is type any

--- src/stores/theme.tsx
+++ src/stores/theme.tsx
@@ -1,10 +1,5 @@
-import { IChildren, ITheme } from "@/utils/types";
-import React, {
-  createContext,
-  useState,
-  useContext,
-  useEffect
-} from "react";
+import type { IChildren, ITheme } from '@/utils/types';
+import React, { createContext, useState, useContext, useEffect } from 'react';
 import CssBaseline from '@mui/material/CssBaseline';
 import { ThemeProvider as MuiProvider } from '@mui/material/styles';
 import * as mui from '@/config/mui-theme';
@@ -15,48 +10,43 @@ interface ThemeContextValue {
   isDark: boolean;
 }
 
-const LOCAL_KEY = "theme-color";
+const LOCAL_KEY = 'theme-color';
 
 const ThemeContext = createContext<ThemeContextValue | undefined>(undefined);
 
 const ThemeProvider = ({ children }: IChildren) => {
-  const [theme, setTheme] = useState<ITheme>(
-    "light"
-  );
+  const [theme, setTheme] = useState<ITheme>('light');
 
   const toggleTheme = () => {
     setTheme((prevTheme) => {
-      const other = prevTheme === "light" ? "dark" : "light";
-      localStorage.setItem(LOCAL_KEY, other)
+      const other = prevTheme === 'light' ? 'dark' : 'light';
+      localStorage.setItem(LOCAL_KEY, other);
       return other;
     });
   };
 
   useEffect(() => {
-    if(typeof window !== "undefined") {
-      const darkModeQuery = window.matchMedia("(prefers-color-scheme: dark)");
-      const prefersDark = darkModeQuery.matches ? "dark" : "light";
-      const localStorageTheme = localStorage.getItem(LOCAL_KEY) as
-        ITheme
-        | null;
-      if(!localStorageTheme) setTheme
-      setTheme(localStorageTheme ?? prefersDark)
+    if (typeof window !== 'undefined') {
+      const darkModeQuery = window.matchMedia('(prefers-color-scheme: dark)');
+      const prefersDark = darkModeQuery.matches ? 'dark' : 'light';
+      const localStorageTheme = localStorage.getItem(
+        LOCAL_KEY,
+      ) as ITheme | null;
+      setTheme(localStorageTheme ?? prefersDark);
     }
-  }, [])
+  }, []);
 
   useEffect(() => {
-    localStorage.setItem("theme-color", theme);
+    localStorage.setItem('theme-color', theme);
   }, [theme]);
 
-  const isDark = theme === "dark";
+  const isDark = theme === 'dark';
 
   return (
-    <ThemeContext.Provider
-      value={{ theme, toggleTheme, isDark }}
-    >
-          <MuiProvider theme={isDark ? mui.darkTheme : mui.lightTheme}>
-          <CssBaseline />
-      {children}
+    <ThemeContext.Provider value={{ theme, toggleTheme, isDark }}>
+      <MuiProvider theme={isDark ? mui.darkTheme : mui.lightTheme}>
+        <CssBaseline />
+        {children}
       </MuiProvider>
     </ThemeContext.Provider>
   );
@@ -65,7 +55,7 @@ const ThemeProvider = ({ children }: IChildren) => {
 const useThemeMode = (): ThemeContextValue => {
   const context = useContext(ThemeContext);
   if (!context) {
-    throw new Error("useTheme must be used within a ThemeProvider");
+    throw new Error('useTheme must be used within a ThemeProvider');
   }
   return context;
 };
