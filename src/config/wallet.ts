f2d482240b03f975c7485a24a66c3e4b222f894b
deepanshu <dipanshuraz2@gmail.com>
2024-07-30 10:07:54 +0530

    add coinbase wallet support

--- src/config/wallet.ts
+++ src/config/wallet.ts
@@ -1,13 +1,17 @@
-import { NETWORK } from '@/utils/constants';
-import { http, createConfig } from 'wagmi';
-import { arbitrum, localhost } from 'wagmi/chains';
-import { injected, walletConnect } from 'wagmi/connectors';
+import { NETWORK } from "@/utils/constants";
+import { http, createConfig } from "wagmi";
+import { arbitrum, localhost } from "wagmi/chains";
+import { injected, walletConnect, coinbaseWallet } from "wagmi/connectors";
 
-const wcProjectId = '10fa7eb32238070e7838c931d185363a';
+const wcProjectId = "10fa7eb32238070e7838c931d185363a";
 
 export const walletConfig = createConfig({
-  chains: NETWORK === 'localhost' ? [localhost] : [arbitrum],
-  connectors: [injected(), walletConnect({ projectId: wcProjectId })],
+  chains: NETWORK === "localhost" ? [localhost] : [arbitrum],
+  connectors: [
+    injected(),
+    walletConnect({ projectId: wcProjectId }),
+    coinbaseWallet({ appName: "Lazarev" }),
+  ],
   transports: {
     [arbitrum.id]: http(),
     [localhost.id]: http(),
--- src/ui/molecules/connect-wallet.tsx
+++ src/ui/molecules/connect-wallet.tsx
@@ -1,31 +1,57 @@
-'use client';
+"use client";
 
-import type { Connector } from 'wagmi';
-import { useAccount, useConnect, useDisconnect } from 'wagmi';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import Button from '@mui/material/Button';
-import Checkbox from '@mui/material/Checkbox';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import CircularProgress from '@mui/material/CircularProgress';
-import Image from 'next/image';
-import type { SyntheticEvent } from 'react';
-import { truncate } from '@/utils/helpers';
-import React from 'react';
-import { Link, Typography } from '@mui/material';
-import Modal from '../atoms/modal';
+import { useAccount, useConnect, useDisconnect } from "wagmi";
+import { useSwitchChain } from "wagmi";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import Button from "@mui/material/Button";
+import Checkbox from "@mui/material/Checkbox";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import CircularProgress from "@mui/material/CircularProgress";
+import Image from "next/image";
+import type { SyntheticEvent } from "react";
+import { truncate } from "@/utils/helpers";
+import React from "react";
+import { Link, Typography } from "@mui/material";
+import Modal from "../atoms/modal";
+import { arbitrum } from "wagmi/chains";
+import { useChainId } from "wagmi";
 
 const TERMS_OF_SERVICE_URL =
-  'https://docs.augustdigital.io/legal/legal-notices/terms-of-service';
+  "https://docs.augustdigital.io/legal/legal-notices/terms-of-service";
 const PRIVACY_POLICY_URL =
-  'https://docs.augustdigital.io/legal/legal-notices/privacy-policy';
+  "https://docs.augustdigital.io/legal/legal-notices/privacy-policy";
 
 type IConnectWallet = {
   btnFullWidth?: boolean;
-  variant?: 'contained' | 'text' | 'outlined';
-  color?: 'primary' | 'inherit';
+  variant?: "contained" | "text" | "outlined";
+  color?: "primary" | "inherit";
   onClose?: (() => void) | undefined;
 };
+
+interface Connector {
+  name: string;
+  id: string;
+}
+
+const connectorsList: Connector[] = [
+  {
+    name: "Metamask",
+    id: "io.metamask",
+  },
+  {
+    name: "Wallet Connect",
+    id: "walletConnect",
+  },
+  {
+    name: "Ledger",
+    id: "ledger",
+  },
+  {
+    name: "Coinbase Wallet",
+    id: "coinbaseWalletSDK",
+  },
+];
 
 const ConnectWalletMolecule = ({
   btnFullWidth,
@@ -36,6 +62,9 @@ const ConnectWalletMolecule = ({
   const { connectors, connectAsync, isPending: connectPending } = useConnect();
   const { disconnect, isPending: disconnectPending } = useDisconnect();
   const { address } = useAccount();
+  const chainId = useChainId();
+  const { switchChain } = useSwitchChain();
+
   const [selectedConnector, setSelectedConnector] =
     React.useState<Connector | null>(null);
   const [agreeToTerms, setAgreeToTerms] = React.useState(false);
@@ -50,25 +79,32 @@ const ConnectWalletMolecule = ({
   async function handleConnect(e: SyntheticEvent) {
     e.preventDefault();
     if (selectedConnector) {
+      const selectedWallet = connectors.find(
+        ({ id }) => id === selectedConnector?.id
+      );
       try {
-        await connectAsync({ connector: selectedConnector });
+        await connectAsync({
+          connector: selectedWallet,
+        });
         if (onClose) {
           onClose();
         }
+        if (chainId !== arbitrum.id) {
+          await switchChain({ chainId: arbitrum.id });
+        }
       } catch (error) {
-        console.error('user rejected request:', error);
+        console.error("user rejected request:", error);
       }
     }
   }
 
   function renderIcon(connector: Connector) {
-    if (connector.icon) return connector.icon;
     return `/wallets/${connector.id?.toLowerCase()}.svg`;
   }
 
   function renderChildren() {
     if (address) return truncate(address);
-    return 'Connect Wallet';
+    return "Connect Wallet";
   }
 
   return (
@@ -76,19 +112,19 @@ const ConnectWalletMolecule = ({
       title="Connect Wallet"
       buttonProps={{
         fullWidth: btnFullWidth ?? false,
-        variant: variant ?? (address ? 'contained' : 'outlined'),
-        color: color ?? (address ? 'primary' : 'inherit'),
-        size: 'large',
+        variant: variant ?? (address ? "contained" : "outlined"),
+        color: color ?? (address ? "primary" : "inherit"),
+        size: "large",
         children: renderChildren(),
         onClick: address ? () => disconnect() : undefined,
       }}
       closeWhen={!!address}
     >
       <List>
-        {connectors
-          .filter((c) => c.id !== 'injected')
-          .map((connector) => (
-            <ListItem key={`connector-${connector.uid}`}>
+        {connectorsList
+          .filter((c) => c.id !== "injected")
+          .map((connector, index) => (
+            <ListItem key={`connector-${index}`}>
               <Button
                 onClick={() => setSelectedConnector(connector)}
                 disabled={connectPending || disconnectPending}
@@ -96,16 +132,15 @@ const ConnectWalletMolecule = ({
                 size="medium"
                 variant={
                   selectedConnector?.id === connector.id
-                    ? 'contained'
-                    : 'outlined'
+                    ? "contained"
+                    : "outlined"
                 }
-                // color={selectedConnector?.id === connector.id ? 'primary' : 'default'}
                 style={{
-                  justifyContent: 'start',
+                  justifyContent: "start",
                   ...(selectedConnector?.id !== connector.id
                     ? {
-                        '&:hover': {
-                          backgroundColor: '#e0e0e0',
+                        "&:hover": {
+                          backgroundColor: "#e0e0e0",
                         },
                       }
                     : {}),
@@ -134,12 +169,12 @@ const ConnectWalletMolecule = ({
               />
             }
             label={
-              <Typography fontSize={'0.875rem'}>
-                I have read and accept{' '}
+              <Typography fontSize={"0.875rem"}>
+                I have read and accept{" "}
                 <Link target="_blank" href={TERMS_OF_SERVICE_URL}>
                   Terms of Service
-                </Link>{' '}
-                and{' '}
+                </Link>{" "}
+                and{" "}
                 <Link target="_blank" href={PRIVACY_POLICY_URL}>
                   Privacy Notice
                 </Link>
@@ -162,7 +197,7 @@ const ConnectWalletMolecule = ({
             color="primary"
             startIcon={connectPending ? <CircularProgress size={24} /> : null}
           >
-            {connectPending ? 'Connecting...' : 'Connect'}
+            {connectPending ? "Connecting..." : "Connect"}
           </Button>
         </ListItem>
       </List>
