d945425a9816b4081a84a6c62af9ba679e98c391
lazarev <lazarevcrypto@gmail.com>
2024-08-09 10:02:26 -0400

    fallback chain id changed to 1

--- src/utils/constants/web3.ts
+++ src/utils/constants/web3.ts
@@ -1,6 +1,7 @@
 export const INFURA_API_KEY = process.env.NEXT_PUBLIC_INFURA_API_KEY || '';
 
 export const NETWORK =
-  (process.env.NEXT_PUBLIC_NETWORK as 'localhost' | 'arbitrum') ?? 'arbitrum';
+  (process.env.NEXT_PUBLIC_NETWORK as 'localhost' | 'arbitrum' | 'mainnet') ??
+  'mainnet';
 
-export const FALLBACK_CHAINID = 42161;
+export const FALLBACK_CHAINID = 1;
