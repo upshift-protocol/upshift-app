9ca72b3808db68ecf1798c9738ed7889af94c180
lazarev <lazarevcrypto@gmail.com>
2024-10-14 10:16:27 -0400

    no typescript check

--- netlify/edge-functions/geolocation.ts
+++ netlify/edge-functions/geolocation.ts
@@ -1,4 +1,5 @@
-import type { Config, Context } from '@netlify/edge-functions';
+// @ts-nocheck
+import type { Context } from '@netlify/edge-functions';
 
 const TERMS_URL = 'https://terms.upshift.finance';
 
@@ -6,9 +7,4 @@ export default async (_request: Request, context: Context) => {
   // block USA
   const country = context.geo.country?.code;
   if (country === 'US') return Response.redirect(TERMS_URL);
-  return new URL(_request.url);
-};
-
-export const config: Config = {
-  path: '/*',
 };
