bdfce1637713b3418a6f1a5de23f99afccb0bd12
lazarev <lazarevcrypto@gmail.com>
2024-10-14 00:27:16 -0400

    return undefined

--- netlify/edge-functions/geolocation.ts
+++ netlify/edge-functions/geolocation.ts
@@ -6,7 +6,6 @@ export default async (_request: Request, context: Context) => {
   // block USA
   const country = context.geo.country?.code;
   if (country === 'US') return Response.redirect(TERMS_URL);
-  return;
 };
 
 export const config: Config = {
