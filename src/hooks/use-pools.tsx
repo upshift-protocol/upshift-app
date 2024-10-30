a965fb56e37c24077b5f8b4fc6a7aa4ab472270e
angello <angello@fractalprotocol.org>
2024-10-30 12:59:42 -0400

    started on optimizing load speed

---
+++ src/hooks/use-pools.tsx
@@ -0,0 +1,32 @@
+import { augustSdk } from '@/config/august-sdk';
+import { buildQueryKey } from '@/utils/helpers/query';
+import type { IAddress, IChainId } from '@augustdigital/sdk';
+import { POOL_ADDRESSES } from '@augustdigital/sdk';
+import { useQueries } from '@tanstack/react-query';
+import { useChainId } from 'wagmi';
+
+interface IUsePools {
+  pool?: IAddress;
+  enabled?: boolean;
+}
+
+export default function usePools(props?: IUsePools) {
+  const chainId = useChainId();
+  const poolAddresses = props?.pool
+    ? [props.pool]
+    : Object.values(POOL_ADDRESSES?.[chainId as IChainId]);
+
+  const queries = useQueries({
+    queries: poolAddresses.map((address) => ({
+      queryFn: () =>
+        augustSdk.pools.getPool(address as IAddress, chainId as IChainId, {
+          allocations: false,
+          loans: false,
+        }),
+      queryKey: buildQueryKey('lending-pool', undefined, address),
+      enabled: props?.enabled || true,
+    })),
+  });
+
+  return queries;
+}
---
+++ src/utils/helpers/array.ts
@@ -0,0 +1,2 @@
+export const arrayAllEqualTrue = (arr: boolean[]) =>
+  arr?.every((val) => val === true);
