991615a60348adf86dda94d9e4072b35849b5151
angello <angello@fractalprotocol.org>
2024-11-12 10:13:06 -0500

    config added to middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,6 +4,11 @@ import { NextResponse } from 'next/server';
 // Block USA
 const BLOCKED_COUNTRIES = ['US'];
 
+// Limit middleware pathname config
+export const config = {
+  matcher: '/',
+};
+
 export default function middleware(
   req: NextRequest & { geo: { country: string } },
 ) {
