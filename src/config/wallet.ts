85717f80fe755d4098086755e16617333626ae24
angello <angello@fractalprotocol.org>
2024-11-22 14:41:35 -0500

    added new base pool

--- src/config/wallet.ts
+++ src/config/wallet.ts
@@ -1,6 +1,6 @@
 import { ACTIVE_NETWORKS, RPC_URLS } from '@/utils/constants/web3';
 import { http, createConfig } from 'wagmi';
-import { localhost, avalanche, mainnet } from 'wagmi/chains';
+import { localhost, avalanche, mainnet, base } from 'wagmi/chains';
 import { injected, walletConnect, coinbaseWallet } from 'wagmi/connectors';
 
 const wcProjectId = '10fa7eb32238070e7838c931d185363a';
@@ -15,6 +15,7 @@ export const walletConfig = createConfig({
   transports: {
     [mainnet.id]: http(RPC_URLS[1]),
     [avalanche.id]: http(RPC_URLS[43114]),
+    [base.id]: http(RPC_URLS[8453]),
     [localhost.id]: http(),
   },
 });
--- src/utils/constants/web3.ts
+++ src/utils/constants/web3.ts
@@ -1,4 +1,4 @@
-import { avalanche, localhost, mainnet } from 'viem/chains';
+import { avalanche, base, localhost, mainnet } from 'viem/chains';
 import { REFERRALS_ENABLED } from './ui';
 
 export const INFURA_API_KEY = process.env.NEXT_PUBLIC_INFURA_API_KEY || '';
@@ -16,6 +16,7 @@ export const RPC_URLS = {
   // 43114: `https://alien-little-hill.avalanche-mainnet.quiknode.pro/ac22aebf0974c406f923035ed5bd499365bf298f/ext/bc/C/rpc/`
   43114: `https://avax-mainnet.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
   1: `https://eth-mainnet.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
+  8453: `https://base-mainnet.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
 };
 
 export const ACTIVE_RPC_URLS = REFERRALS_ENABLED
@@ -25,11 +26,12 @@ export const ACTIVE_RPC_URLS = REFERRALS_ENABLED
   : {
       1: RPC_URLS[1],
       43114: RPC_URLS[43114],
+      8453: RPC_URLS[8453],
     };
 
 export const ACTIVE_NETWORKS =
   NETWORK === 'localhost'
-    ? [mainnet, avalanche, localhost]
+    ? [mainnet, avalanche, base, localhost]
     : REFERRALS_ENABLED
       ? [avalanche]
-      : [mainnet, avalanche];
+      : [mainnet, avalanche, base];
