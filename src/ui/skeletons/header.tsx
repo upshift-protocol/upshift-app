84d6639aeaa301cc1043204fed276174fbf4f696
angello <angello@fractalprotocol.org>
2024-12-05 09:51:38 -0500

    added referral code block on ethena

--- src/ui/skeletons/header.tsx
+++ src/ui/skeletons/header.tsx
@@ -5,7 +5,7 @@ import Box from '@mui/material/Box';
 import Stack from '@mui/material/Stack';
 import Toolbar from '@mui/material/Toolbar';
 
-import { REFERRALS_ENABLED, STYLE_VARS } from '@/utils/constants';
+import { INSTANCE, REFERRALS_ENABLED, STYLE_VARS } from '@/utils/constants';
 import { useThemeMode } from '@/stores/theme';
 import { Button, Drawer, IconButton, Skeleton } from '@mui/material';
 import { useState } from 'react';
@@ -91,7 +91,7 @@ const HeaderSkeleton = () => {
                 <Logo />
               </LinkAtom>
 
-              {REFERRALS_ENABLED ? (
+              {REFERRALS_ENABLED && INSTANCE !== 'ethena' ? (
                 <Box display={{ xs: 'none', sm: 'flex' }}>
                   <MyReferralsModalMolecule />
                 </Box>
